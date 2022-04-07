dbe84112df9f1845e640dff6917d50f8adeaff14
Nelson Mills <nelsonmills000@gmail.com>
2022-04-07 02:36:34 -0500

    updated portfolio page

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 // import { getAllEvents } from "../../solana/functions";
 import { Event } from "../../solana/models";
-import { Button, Card, CardContent, Typography, Grid, TextField, Stack } from '@mui/material';
+import { Button, Card, CardContent, Typography, Grid, TextField, Stack, Box } from '@mui/material';
+
 
 export function Portfolio() {
     const [events, setEvents] = useState<Event[]>([]);
     let navigate = useNavigate();
 
-    let bought: number[] = [0.25, 0.50, 0.84,0.25, 0.50, 0.84];
-    let current: number[] = [0.56, 0.43, 0.90, 0.56, 0.43, 0.90];
+    let bought: number[] = [0.25, 0.50, 0.84];
+    let current: number[] = [0.56, 0.43, 0.90];
+    let tokens: number[] = [1, 5, 3]
+    let positions: string[] = ["yes", "no", "yes"]
 
     useEffect(() => {
         fetch(`${process.env.REACT_APP_API_URL}/events`).then(res => {
@@ -26,32 +29,92 @@ export function Portfolio() {
 
     return (
         <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: '30px', width: '70%' }}>
-            <Button variant="contained" color="secondary" onClick={() => navigate('/newevent')}>Request New Event</Button>
 
-            {events.length ? <Stack>
-                {events.length > 0 && events.map(e =>
+            {events.length ? <Stack spacing={1}>
+                {events.length > 0 && events.map((e, index) =>
                     <div key={e.name}>
-                        <Card variant="outlined" style={{ margin: "40px", padding: "20px", borderRadius: '20px' }}>
+                        <Card variant="outlined" style={{ padding: "5px", borderRadius: '10px' }}>
                             <CardContent>
-                                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                                <Stack spacing={2} direction='row' style={{ marginRight: '20px', marginBottom: '20px' , textAlign:'center'}}>
                                     <img src={e.imageLink} style={{ objectFit: 'cover', width: '35px', height: '35px', borderRadius: '100%', marginRight: '10px' }}></img>
-                                    <Typography variant="h5" component="div">
+                                    <Typography variant="body1" component="div">
                                         {e.name}
                                     </Typography>
-                                    <Typography variant="body2">
-                                        {"Resolves on " + e.resolutionDate}
-                                    </Typography>
-                                    <Typography variant="body2">
-                                        Bought Price
-                                    </Typography>
-                                    <Typography variant="body2">
-                                        Current Price
-                                    </Typography>
-                                    <Typography variant="body2">
-                                        Profit
-                                    </Typography>
-                                    <Button onClick={() => navigate(`/event/${e.id!}`)} style={{ marginTop: '20px' }} variant="contained">TRADE</Button>
-                                </div>
+                                </Stack>
+                                <Stack spacing={2} direction="row" style={{ float: 'right', marginBottom: "20px" }}>
+                                    <Stack spacing={1} style={{ textAlign: "center" }}>
+                                        <Typography variant="body2">
+                                            Position
+                                        </Typography>
+                                        <Typography variant="body2">
+                                            {positions[index % 3]}
+                                        </Typography>
+                                    </Stack>
+
+                                    <Stack spacing={1} style={{ textAlign: "center" }}>
+                                        <Typography variant="body2">
+                                            Resolution Date
+                                        </Typography>
+                                        <Typography variant="body2">
+                                            {e.resolutionDate}
+                                        </Typography>
+                                    </Stack>
+                                    <div className='element'>
+                                        <Stack spacing={1} style={{ textAlign: "center" }}>
+                                            <Typography variant="body2">
+                                                Bought Price
+                                            </Typography>
+                                            <Typography variant="body2">
+                                                {bought[index % 3].toFixed(2)}
+                                            </Typography>
+                                        </Stack>
+                                    </div>
+                                    <div className='element'>
+                                        <Stack spacing={1} style={{ textAlign: "center" }}>
+                                            <Typography variant="body2">
+                                                Current Price
+                                            </Typography>
+                                            <Typography variant="body2">
+                                                {current[index % 3].toFixed(2)}
+                                            </Typography>
+                                        </Stack>
+                                    </div>
+                                    <div className='element'>
+                                        <Stack spacing={1} style={{ textAlign: "center" }}>
+                                            <Typography variant="body2">
+                                                Tokens Owned
+                                            </Typography>
+                                            <Typography variant="body2">
+                                                {tokens[index % 3]}
+                                            </Typography>
+                                        </Stack>
+                                    </div>
+                                    <div className='element'>
+                                        <Stack spacing={1} style={{ textAlign: "center" }}>
+                                            <Typography variant="body2">
+                                                Profit/Loss
+                                            </Typography>
+                                            <Typography variant="body2">
+
+                                                {
+                                                    (tokens[index] * (current[index] - bought[index])) < 0 ?
+                                                        <Typography variant="body2" style={{ color: "red" }}>
+                                                            {(tokens[index % 3] * (current[index % 3] - bought[index % 3])).toFixed(2)}
+                                                        </Typography> :
+                                                        <Typography variant="body2" style={{ color: "green" }}>
+                                                            {(tokens[index % 3] * (current[index % 3] - bought[index % 3])).toFixed(2)}
+                                                        </Typography>
+                                                }
+
+
+                                            </Typography>
+                                        </Stack>
+
+                                    </div>
+                                    <Button color="secondary" onClick={() => navigate(`/event/${e.id!}`)} style={{ marginTop: '10px' }} variant="contained">TRADE</Button>
+
+                                </Stack>
+
                             </CardContent>
                         </Card>
                     </div>
@@ -61,3 +124,8 @@ export function Portfolio() {
     );
 }
 
+function getRandomNum(): number {
+    var min: number = Math.ceil(0);
+    var max: number = Math.floor(100);
+    return (Math.floor(Math.random() * (max - min + 1)) + min) / 100;
+}
